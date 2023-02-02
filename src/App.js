ab3829776a863d1054f894bc6ca70fcc3ad7ae54
RastkoAmun <raciosh@gmail.com>
2023-02-01 17:58:26 -0800

    Upgraded input handler for both adding name and numbers

--- src/App.js
+++ src/App.js
@@ -3,42 +3,62 @@ import ContactForm from './components/ContactFrom';
 
 const App = () => {
   const [contacts, setContacts] = useState([
-    {name: 'Magnus Carlsen'},
-    {name: 'Anish Giri'}
+    {name: 'Magnus Carlsen', number: '236-455-8776'},
+    {name: 'Anish Giri', number: '665-009-2341'},
+    {name: 'Hikaru Nakamura', number: '778-665-1157'}
   ])
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   //Function for control changing state of form input
   const handleInput = (event) => {
-    setNewName(event.target.value)
+    if(event.target.className === 'name'){
+      setNewName(event.target.value)
+    }else{
+      setNewNumber(event.target.value)
+    }
   }
 
+  //Function for adding a new contact in the list
   const addContact = (event) => {
     event.preventDefault()
     const newContact = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
-    setContacts(contacts.concat(newContact))
-    setNewName('')
-  }
 
+    const isFound = contacts.find(contact => 
+      contact.name === newName)
+    
+    if(isFound){
+      alert(`${newName} is already in your contacts!`)
+      setNewName('')
+    }else{
+      setContacts(contacts.concat(newContact))
+      setNewName('')
+      setNewNumber('')
+    }
+  }
 
   return(
     <div>
       <h2>Phonebook</h2>
-      {/* <form>
+      <form>
         <div>
-          name: <input value={newName} onChange={handleInput}/>
+          name: <input className='name' value={newName} onChange={handleInput}/>
+        </div>
+        <div>
+          number: <input className='number' value={newNumber} onChange={handleInput}/>
         </div>
         <div>
           <button type='submit' onClick={addContact}>add</button>
         </div>
-      </form> */}
-      <ContactForm newName={newName} addContact={addContact} handleInput={handleInput} />
+      </form>
+      {/* <ContactForm newName={newName} addContact={addContact} handleInput={handleInput} /> */}
         <h2>Numbers</h2>
         <div>
           {contacts.map(contact => 
-            <div>{contact.name}</div>
+            <div key={contact.number}>{contact.name}: {contact.number}</div>
           )}
         </div>
     </div>
